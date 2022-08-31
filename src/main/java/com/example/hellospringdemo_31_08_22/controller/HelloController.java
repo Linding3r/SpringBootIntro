eab36345ca774178e31fdca7d5ce2afaeb548b65
Linding3r <thomasjuullindinger@yahoo.dk>
2022-08-31 11:19:49 +0200

    added /isitfriday

--- src/main/java/com/example/hellospringdemo_31_08_22/controller/HelloController.java
+++ src/main/java/com/example/hellospringdemo_31_08_22/controller/HelloController.java
@@ -1,8 +1,12 @@
 package com.example.hellospringdemo_31_08_22.controller;
 
+import org.springframework.cglib.core.Local;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.DayOfWeek;
+import java.time.LocalDateTime;
 
 @RestController
 public class HelloController {
@@ -17,9 +21,16 @@ public class HelloController {
     return "Hello Motherfucker";
   }
 
-  @GetMapping("/parametertest")
-  public String parameter(@RequestParam String mytext, @RequestParam String mySecondText){
-    return mytext + " | " + mySecondText;
+  @GetMapping("/echo")
+  public String parameter(@RequestParam String mytext){
+    return mytext;
   }
 
+  @GetMapping("/isitfriday")
+  public String isfriday(){
+    String output = "If it friday today?";
+    if(LocalDateTime.now().getDayOfWeek().equals(DayOfWeek.FRIDAY))
+      return output+= "  YES";
+    else return output+= "  NO";
+  }
 }
